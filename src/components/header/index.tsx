e5d60f73406d5ed839231945a3e8aee376460a4b
gustavo lima <gustavo.lima.sj1@gmail.com>
2024-10-17 12:08:33 -0300

    mudança head

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,23 +1,48 @@
+"use client"
 import Link from "next/link"
-
-import { FiUser, FiLogOut } from "react-icons/fi"
+import { signIn, signOut, useSession } from "next-auth/react"
+import { FiUser, FiLogOut, FiLoader } from "react-icons/fi"
+import { BiLogIn } from "react-icons/bi";
 export function Header() {
+    async function handleLogin() {
+        await signIn()
+    }
+
+    async function handleLogout() {
+        await signOut()
+    }
+    const { status, data } = useSession()
     return (
         <header className="w-full px-4 py-4">
             <div className="flex w-full mx-auto justify-between shadow-sm py-2 px-1">
+
                 <h1 className="font-bold text-2xl pl-1 hover:tracking-widest transition-all duration-3000">
                     <span className=" text-violet-700">dev</span>controler
                 </h1>
 
-                <div className="flex justify-center items-center gap-4">
-                    <Link href="/">
-                        <FiUser size={22} />
-                    </Link>
+                {status === "unauthenticated" && (
+                    <div>
+                        <button onClick={handleLogin}>
+                            <BiLogIn size={22} />
+                        </button>
+                    </div>
+                )}
 
-                    <button>
-                        <FiLogOut size={22} />
-                    </button>
-                </div>
+                {status === "loading" && (
+                    <FiLoader size={22} color="purple" />
+                )}
+
+                {status === "authenticated" && (
+                    <div className="flex justify-center items-center gap-4">
+                        <Link href="/">
+                            <FiUser size={22} />
+                        </Link>
+
+                        <button>
+                            <FiLogOut size={22} />
+                        </button>
+                    </div>
+                )}
             </div>
         </header>
     )
